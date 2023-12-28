eeedaa0f38f5fca37390bd1ff6f801325eabe2dc
Tyson Schwartz <Scratelt@yahoo.com>
2023-12-28 14:26:44 -0500

    added password validaiton and aesthetics

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 // JS
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const form = document.querySelector("form");
 
+
+function checkPassword(event) {
+    const isError = passwordInput.value.length < 8 && passwordInput.value.trim().length > 0;
+
+    if (isError) {
+        document.getElementById("password").classList.add("error");
+        return false;
+    } else {
+        document.getElementById("password").classList.remove("error");
+        return true;
+    }
+};
+
+
+function handleSubmit(event) {
+    if (!checkPassword()) {
+    event.preventDefault();
+    alert("Password must be at least 8 characters long");
+    } else {
+    const password = passwordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        event.preventDefault();
+
+    }
+}}
+
+passwordInput.addEventListener("input", checkPassword);
+confirmPasswordInput.addEventListener("input", checkPassword);
+form.addEventListener("submit", handleSubmit);
